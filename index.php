5c06135e0caf73edb1a6f541a94aab1eb787c472
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-08-22 15:56:26 +0300

    added styles and template of main page

--- index.php
+++ index.php
@@ -23,6 +23,43 @@
         exit('Page 404');
     }
 
+
+
+    function top($title) {
+        echo '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>'.$title.'</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
+        
+        <div class="wrapper">
+                <div class="menu">
+                    <a href="/">Главная</a>
+                    <a href="/login">Вход</a>
+                    <a href="/register">Регистрация</a>
+                </div>
+                    <div class="content">
+                        <div class="block">
+
+
+                
+        ';
+    }
+
+    function bottom(){
+        echo '
+                        </div>
+                    </div>
+                </div>
+            </body>
+            </html>';
+    }
+
     
 
 
