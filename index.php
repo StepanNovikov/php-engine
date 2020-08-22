cf48c4c883861e239165dbd9e68aee1f4f2f560f
StepanNovikov <stepan.novikov.1996@gmail.com>
2020-08-22 14:12:21 +0300

    adding pages

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php   
+    if($_SERVER['REQUEST_URI'] == '/php-engine/') {
+        $page = 'home';
+    } else {
+        $page = substr($_SERVER['REQUEST_URI'],12);
+        if(!preg_match('/^[A-z0-9]{3,15}$/',$page)){
+            exit('error url');
+        }
+    }
+
+    session_start();
+
+    if (file_exists("all/".$page.".php")) {
+        include "all/".$page.".php";
+    } 
+    else if($_SESSION['ulogin'] == 1 and file_exists("auth/".$page.".php")) {
+        include "auth/".$page.".php";
+    } 
+    else if($_SESSION['ulogin'] !=1 and file_exists("guest/".$page.".php")) {
+        include "guest/".$page.".php"; 
+    } 
+    else {
+        exit('Page 404');
+    }
+
+    
+
+
+
+
+
+
+
+?>
